20a50b9acf25690efa1e0a3f6e3a8740ca555d8e
MariaKuusvek <2020mariaku@gmail.com>
2020-11-29 01:04:27 +0200

    Sain esimese slaidi tööle!!! + komenteerisin javascripti

--- javapea.js
+++ javapea.js
@@ -1,20 +1,32 @@
+// Slideshow tutorial: https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+// Defineerime muutujat, nii et esimene slaid mis näidatakse on nr 1
 var slideIndex = 1;
 
+// Defineerime funktsiooni, mis vahetab millise slaidi peale läheme, ja kutsume välja funktsiooni mis näitab slaide
+// Oleneb kas vajutasid vasakut noolt, n = -1, või paremat noolt, n = 1
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
+// Defineerime funktsiooni, mis kutsub välja funktsiooni mis näitab slaide
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
+// Funktsioon, mis näitab slaide muutuja n-i järgi. 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
+	// Defineerime muutujad. Slides on kõik elemendid minu HTML koodis mis on "slide" div classi vahel
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	// Kui n on suurem kui meie slaidide koguarv, siis loopib tagasi esimese slaidi peale
+	if (n > slides.length) {slideIndex = 1}    
+	// Kui n on väiksem kui 1, siis loopib viimase slaidi peale
+	if (n < 1) {slideIndex = slides.length}
+	// For loop, mis ütleb et mitte näidata kõike slaide korraga
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	// Ainult näitab seda ühte slaidi, mille peal me oleme
+	slides[slideIndex-1].style.display = "block";  
 }
--- lisaleht.js
+++ lisaleht.js
@@ -1,9 +1,14 @@
-//Google maps tutorial: https://www.w3schools.com/graphics/google_maps_basic.asp
+// Google maps tutorial: https://www.w3schools.com/graphics/google_maps_basic.asp
 
+// Defineerin funktsiooni
 function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	
+	// Kuna selle kasutamiseks on vaja raha maksta, siis preagu on kordinaadid pandud (0,0)
+	// Map on preaguseks ilustreeriv. Kui maksame raha, siis saab kordinaadid juurde panna ja näidata meie kontori asukohta
+	var mapProp= {
+	center:new google.maps.LatLng(0,0),
+	zoom:5,
+	};
+	
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
